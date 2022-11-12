d20b346235c5c04cd31cab221b27cdf905f66e22
paulohyst <oluaptarsso@gmail.com>
2022-11-11 21:15:10 -0300

    refactor(config): added configs to eslint, prettier, package and tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "max-len": ["error", {"code": 2000, "ignoreUrls": true}],
     'prettier/prettier': [
       'error',
       {
