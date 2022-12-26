deb12d6ec188ac7cdf2d0765e787b6817c03cf77
paulohyst <oluaptarsso@gmail.com>
2022-12-26 19:45:14 -0300

    refactor(prettier)

--- src/music/music.service.ts
+++ src/music/music.service.ts
@@ -6,10 +6,7 @@ import { join } from 'path';
 @Injectable()
 export class MusicAppService {
   async getMusics(): Promise<MusicDTO[]> {
-    const rawData = await readFile(
-      join(process.cwd(), 'src/data/data.json'),
-      'utf-8',
-    );
+    const rawData = await readFile(join(process.cwd(), 'src/data/data.json'), 'utf-8');
 
     return JSON.parse(rawData) as MusicDTO[];
   }
