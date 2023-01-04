dd2389f9f6a94c12d5e7b898b7f0146f6f46e079
oluaptarso <oluaptarsso@gmail.com>
2023-01-03 22:59:05 -0300

    feat(env): add .env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MusicController } from './music/music.controller';
 import { MusicAppService } from './music/music.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, MusicController],
   providers: [AppService, MusicAppService],
 })
